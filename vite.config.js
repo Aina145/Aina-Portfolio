3131a47634edcab736c1b3c2ba7623742c3c0856
Aina malik <Ainamalik145@gmail.com>
2026-03-28 13:07:09 +0500

    fix: base path casing to Aina-Portfolio

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/Aina-portfolio/",
+  base: "/Aina-Portfolio/",
   server: {
     host: "::",
     port: 8080,
